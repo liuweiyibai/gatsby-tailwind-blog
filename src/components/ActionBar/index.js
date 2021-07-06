05796d7595568d481e1e7bd2989ebf18c3cefa5e
hotblin <lw1140@163.com>
2021-07-06 20:09:12 +0800

    feat: 首页增加过滤

--- src/components/ActionBar/index.js
+++ src/components/ActionBar/index.js
@@ -1,81 +1,68 @@
-import React, { useEffect, useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import screenfull from 'screenfull';
-import { SET_SCROLL_TO_TOP, SET_ASIDER_SHOW } from '@/store';
-import IconBtn from '../IconButton';
-import CategoryFilter from './CategoryFilter';
-import {
-  MdHome,
-  MdSearch,
-  MdFullscreen,
-  MdFullscreenExit,
-  MdArrowUpward,
-  MdDashboard,
-} from 'react-icons/md';
+import React, { useEffect, useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import screenfull from 'screenfull'
+import { SET_SCROLL_TO_TOP, SET_ASIDER_SHOW } from '@/store'
+import IconBtn from '../IconButton'
+import CategoryFilter from './CategoryFilter'
+import { MdHome, MdSearch, MdFullscreen, MdFullscreenExit, MdArrowUpward, MdDashboard } from 'react-icons/md'
 
 // >1024 时显示在右侧，<1024 时在底部
 export default ({ uri, navigate }) => {
-  const dispatch = useDispatch();
-  const [fullscreen, setFullscreen] = useState(false);
-  const categoryFilterShow = useSelector(state => state.categoryFilterShow);
-  const asiderShow = useSelector(state => state.asiderShow);
+  const dispatch = useDispatch()
+  const [fullscreen, setFullscreen] = useState(false)
+  const categoryFilterShow = useSelector(state => state.categoryFilterShow)
+  const asiderShow = useSelector(state => state.asiderShow)
 
   const handleHomeClick = () => {
-    navigate('/');
-  };
+    navigate('/')
+  }
 
   const handleScrollToTop = () => {
     dispatch({
       type: SET_SCROLL_TO_TOP,
-      payload: true,
-    });
-  };
+      payload: true
+    })
+  }
 
   const handleToggleFullScreen = () => {
     if (screenfull.isEnabled) {
-      screenfull.toggle();
+      screenfull.toggle()
     }
-  };
+  }
 
   const handleSetAsider = () => {
     dispatch({
       type: SET_ASIDER_SHOW,
-      payload: !asiderShow,
-    });
-  };
+      payload: !asiderShow
+    })
+  }
 
   useEffect(() => {
     if (screenfull.isEnabled) {
       screenfull.on('change', () => {
-        setFullscreen(screenfull.isFullscreen);
-      });
+        setFullscreen(screenfull.isFullscreen)
+      })
     }
     return () => {
       if (screenfull.isEnabled) {
-        screenfull.off('change');
+        screenfull.off('change')
       }
-    };
-  }, []);
+    }
+  }, [])
 
   const styles = () => {
     if (asiderShow) {
-      return { transform: 'rotate(90deg)' };
+      return { transform: 'rotate(90deg)' }
     }
-  };
+  }
   return (
     <div className="action-bar__wrapper">
       <div className="action-bar__content">
-        <IconBtn
-          aria-label="返回文章列表"
-          title="返回文章列表"
-          onClick={handleHomeClick}
-        >
+        <IconBtn aria-label="返回文章列表" title="返回文章列表" onClick={handleHomeClick}>
           <MdHome />
         </IconBtn>
 
-        {((categoryFilterShow && asiderShow) || uri === '/blog') && (
-          <CategoryFilter />
-        )}
+        {((categoryFilterShow && asiderShow) || uri === '/') && <CategoryFilter />}
 
         <IconBtn onClick={() => navigate('/search')}>
           <MdSearch />
@@ -85,7 +72,7 @@ export default ({ uri, navigate }) => {
           className="hide-icon-button"
           onClick={handleSetAsider}
           style={{
-            ...styles(),
+            ...styles()
           }}
         >
           <MdDashboard />
@@ -93,17 +80,11 @@ export default ({ uri, navigate }) => {
       </div>
       <div className="action-bar__content">
         {/* 缺少一个字体大小调节 */}
-        <IconBtn onClick={handleToggleFullScreen}>
-          {fullscreen ? <MdFullscreenExit /> : <MdFullscreen />}
-        </IconBtn>
-        <IconBtn
-          aria-label="返回顶部"
-          onClick={handleScrollToTop}
-          title="返回顶部"
-        >
+        <IconBtn onClick={handleToggleFullScreen}>{fullscreen ? <MdFullscreenExit /> : <MdFullscreen />}</IconBtn>
+        <IconBtn aria-label="返回顶部" onClick={handleScrollToTop} title="返回顶部">
           <MdArrowUpward />
         </IconBtn>
       </div>
     </div>
-  );
-};
+  )
+}
